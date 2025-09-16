6d8f43bfb5b914f6864f8cff10bddeb9791464f1
Pongpanot Chantana <75673766+munichPMN@users.noreply.github.com>
2025-09-16 23:33:53 +0700

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,15 +1,23 @@
 export const checkPassword = (req, res) => {
-  const { password } = req.body;
-  if(password === "1234"){
-    res.json({ valid:true });
-  } else {
-    res.json({ valid:false });
-  }
+	const {
+		password
+	} = req.body;
+	if (password === "1234") {
+		res.json({
+			valid: true
+		});
+	} else {
+		res.json({
+			valid: false
+		});
+	}
 };
 
-// middleware สำหรับเช็ครหัสผ่านทุกครั้ง
+// middleware สำหรับเช็ครหัสผ่านในส่วนที่มาจากทางฝั่งแอดมิน
 export const authMiddleware = (req, res, next) => {
-  const pwd = req.headers["x-password"];
-  if(pwd !== "1234") return res.status(401).json({ error: "รหัสผ่านไม่ถูกต้อง" });
-  next();
+	const pwd = req.headers["x-password"];
+	if (pwd !== "1234") return res.status(401).json({
+		error: "รหัสผ่านไม่ถูกต้อง"
+	});
+	next();
 };
